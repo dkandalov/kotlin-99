e54838df7fed176183aa36bc683618e07d37dbd7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-09-09 13:28:01 +0100

    hasSameElementsAs() refactoring

--- src/org/kotlin99/P26.kt
+++ src/org/kotlin99/P26.kt
@@ -21,17 +21,17 @@ private fun <T> List<T>.flatMapTails(f: (List<T>) -> (List<List<T>>)): List<List
 class P26Test {
     @Test fun `generate the combinations of K distinct objects chosen from the N elements of a list`() {
         assertThat(combinations(0, "abc".toList()), equalTo(listOf(emptyList())))
-        assertThat(combinations(1, "abc".toList()), hasSameElements(listOf(
+        assertThat(combinations(1, "abc".toList()), hasSameElementsAs(listOf(
                 "a".toList(), "b".toList(), "c".toList()
         )))
-        assertThat(combinations(2, "abc".toList()), hasSameElements(listOf(
-                "cb".toList(), "ba".toList(), "ca".toList()
+        assertThat(combinations(2, "abc".toList()), hasSameElementsAs(listOf(
+                "cb".toList(), "ab".toList(), "ca".toList()
         )))
-        assertThat(combinations(3, "abc".toList()), hasSameElements(listOf(
+        assertThat(combinations(3, "abc".toList()), hasSameElementsAs(listOf(
                 "cba".toList()
         )))
 
-        assertThat(combinations(3, "abcde".toList()), hasSameElements(listOf(
+        assertThat(combinations(3, "abcde".toList()), hasSameElementsAs(listOf(
                 "cba".toList(), "dba".toList(), "eba".toList(),
                 "dca".toList(), "eca".toList(), "eda".toList(),
                 "dcb".toList(), "ecb".toList(), "edb".toList(),
@@ -41,18 +41,50 @@ class P26Test {
     }
 }
 
-fun <T> hasSameElements(iterable: Iterable<T>) : Matcher<Iterable<T>> {
+class SameElementsMatcherTest {
+    @Test fun `non-nested lists`() {
+        assertThat(emptyList<Int>(), hasSameElementsAs(emptyList()))
+        assertThat(listOf(1), hasSameElementsAs(listOf(1)))
+
+        assertThat(listOf(1, 1), !hasSameElementsAs(listOf(1)))
+        assertThat(listOf(1, 2), hasSameElementsAs(listOf(2, 1)))
+
+        assertThat(listOf(1, 2, 3), hasSameElementsAs(listOf(3, 2, 1)))
+        assertThat(listOf(1, 2, 3), !hasSameElementsAs(listOf(4, 2, 1)))
+    }
+
+    @Test fun `nested lists`() {
+        assertThat(listOf(emptyList<Int>()), hasSameElementsAs(listOf(emptyList())))
+        assertThat(listOf(listOf(1)), hasSameElementsAs(listOf(listOf(1))))
+
+        assertThat(listOf(listOf(1, 1)), !hasSameElementsAs(listOf(listOf(1))))
+        assertThat(listOf(listOf(1, 2)), hasSameElementsAs(listOf(listOf(2, 1))))
+
+        assertThat(listOf(listOf(1, 2, 3), listOf(1)), hasSameElementsAs(listOf(listOf(1), listOf(3, 2, 1))))
+        assertThat(listOf(listOf(1, 2, 4), listOf(1)), hasSameElementsAs(listOf(listOf(1), listOf(4, 2, 1))))
+    }
+}
+
+fun <T> hasSameElementsAs(expected: Iterable<T>) : Matcher<Iterable<T>> {
     return object : Matcher.Primitive<Iterable<T>>() {
         override fun invoke(actual: Iterable<T>): MatchResult {
             val actualList = actual.toList()
-            val expectedList = iterable.toList()
-            if (actualList.size == expectedList.size && actualList.toSet() == expectedList.toSet()) {
-                return MatchResult.Match
-            } else {
-                return MatchResult.Mismatch("was ${describe(actual)}")
-            }
+            val expectedList = expected.toList()
+            val isMatch =
+                if (actualList.size != expectedList.size) {
+                    false
+                } else if (actualList.isNotEmpty() && actualList.first() is Iterable<*> && expectedList.first() is Iterable<*>) {
+                    actualList.all { actualValue ->
+                        expectedList.any{ expectedValue ->
+                            hasSameElementsAs(expectedValue as Iterable<*>).invoke(actualValue as Iterable<*>) == MatchResult.Match
+                        }
+                    }
+                } else {
+                    actualList.toSet() == expectedList.toSet()
+                }
+            return if (isMatch) MatchResult.Match else MatchResult.Mismatch("was ${describe(actual)}")
         }
-        override val description: String get() = "has the same elements as ${describe(iterable)}"
-        override val negatedDescription : String get() = "element are not the same as in ${describe(iterable)}"
+        override val description: String get() = "has the same elements as ${describe(expected)}"
+        override val negatedDescription : String get() = "element are not the same as in ${describe(expected)}"
     }
 }
