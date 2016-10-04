f0fbd1533215ec98039b4d54a2ba53fd6e1b58fb
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-10-04 12:32:01 +0100

    refactoring

--- src/org/kotlin99/P50.kt
+++ src/org/kotlin99/P50.kt
@@ -3,23 +3,25 @@ package org.kotlin99
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.Test
-import org.kotlin99.P50.huffman
+import org.kotlin99.P50.huffmanCodes
 
 
 // Use object as namespace to avoid conflicting names.
 object P50 {
 
-    fun huffman(charAndFreq: List<Pair<Char, Int>>): List<Pair<Char, String>> {
-        val sortedNodes = charAndFreq.map{ Node(it.second, it.first) }.sortedBy{ it.weight }
-        val tree = buildTree(sortedNodes)
+    fun huffmanCodes(charAndFreq: List<Pair<Char, Int>>): List<Pair<Char, String>> {
+        val nodes = charAndFreq.map{ Node(it.second, it.first) }
+        val tree = buildTree(nodes)
         return charAndFreq.map{ Pair(it.first, tree.findPath(it.first)!!.reversed()) }
     }
 
     private fun buildTree(nodes: List<Node>): Node =
-            if (nodes.size == 1) nodes.first()
-            else {
-                val node = Node(nodes[0].weight + nodes[1].weight, null, nodes[0], nodes[1])
-                buildTree((nodes.drop(2) + node).sortedBy{ it.weight })
+            if (nodes.size == 1) {
+                nodes.first()
+            } else {
+                val sortedNodes = nodes.sortedBy{ it.weight }
+                val node = Node(sortedNodes[0].weight + sortedNodes[1].weight, null, sortedNodes[0], sortedNodes[1])
+                buildTree(sortedNodes.drop(2) + node)
             }
 
     private data class Node(val weight: Int, val char: Char?, val left: Node? = null, val right: Node? = null) {
@@ -41,7 +43,7 @@ object P50 {
 class P50Test {
     @Test fun `Huffman encoding`() {
         assertThat(
-            huffman(listOf(Pair('a', 45), Pair('b', 13), Pair('c', 12), Pair('d', 16), Pair('e', 9), Pair('f', 5))),
+            huffmanCodes(listOf(Pair('a', 45), Pair('b', 13), Pair('c', 12), Pair('d', 16), Pair('e', 9), Pair('f', 5))),
             equalTo(listOf(Pair('a', "0"), Pair('b', "101"), Pair('c', "100"), Pair('d', "111"), Pair('e', "1101"), Pair('f', "1100")))
         )
     }
