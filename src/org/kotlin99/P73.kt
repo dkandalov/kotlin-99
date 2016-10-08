e0a17969af02b5840ffa25e7761cfca37d722cf8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-10-08 11:47:09 +0100

    use Token? instead of ParsResult; refactoring

--- src/org/kotlin99/P73.kt
+++ src/org/kotlin99/P73.kt
@@ -10,13 +10,21 @@ fun MTree<*>.toLispString(): String =
         else "(" + value.toString() + " " + children.joinToString(" "){ it.toLispString() } + ")"
 
 fun String.fromLispString(): MTree<String> =
-        SExprParser.parse(this).token!!.toMTree()
+        SExprParser.parse(this)!!.trim()!!.toMTree()
 
 
 fun Token.toMTree(): MTree<String> =
         if (this is Seq) {
-            val atom = tokens.filter{ it is Atom }.map{ (it as Atom).value }.first()
-            val children = tokens.filter{ it is Seq }.flatMap{ (it as Seq).tokens }.map{ it.toMTree() }
+            val atom = (tokens.first() as Atom).value
+            val children = tokens.drop(1).flatMap{ token ->
+                if (token is Seq) {
+                    token.tokens.map{ it.toMTree() }
+                } else if (token is Atom) {
+                    listOf(MTree(token.value))
+                } else {
+                    emptyList()
+                }
+            }
             MTree(atom, children)
         } else if (this is Atom) {
             MTree(value)
@@ -26,98 +34,90 @@ fun Token.toMTree(): MTree<String> =
 
 
 interface Token {
-    fun canDrop(): Boolean
+    fun length(): Int
+    fun trim(): Token?
 }
 data class Text(val value: String) : Token {
     override fun toString() = "'$value'"
-    override fun canDrop() = true
+    override fun trim() = null
+    override fun length() = value.length
 }
 data class Atom(val value: String): Token {
     override fun toString() = "v'$value'"
-    override fun canDrop(): Boolean = false
+    override fun trim() = this
+    override fun length() = value.length
 }
 data class Seq(val tokens: List<Token>): Token {
     constructor (vararg tokens: Token) : this(tokens.toList())
     fun prepend(token: Token): Seq = Seq(listOf(token) + tokens)
-    override fun canDrop() = tokens.isEmpty()
+    override fun trim(): Token? =
+        if (tokens.isEmpty()) null
+        else {
+            val trimmed = tokens.map{ it.trim() }.filter{ it != null }.map{ it!! }
+            if (trimmed.size == 1) trimmed.first() else Seq(trimmed)
+        }
     override fun toString() = "Seq[" + tokens.joinToString(" ") + "]"
+    override fun length() = tokens.sumBy{ it.length() }
 }
 
 
 interface TokenParser {
-    fun parse(s: String): ParseResult<Token>
+    fun parse(s: String): Token?
 }
-data class ParseResult<out T : Token>(val token: T?, val s: String)
-fun <T : Token> okParseResult(token: T, s: String): ParseResult<T> = ParseResult(token, s)
-fun failedParseResult(s: String): ParseResult<Nothing> = ParseResult(null, s)
-
 class TextParser(val value: String): TokenParser {
-    override fun parse(s: String): ParseResult<Token> =
-            if (s.startsWith(value)) okParseResult(Text(value), s.drop(1))
-            else failedParseResult(s)
+    override fun parse(s: String): Token? =
+            if (s.startsWith(value)) Text(value) else null
 }
 val LParenParser = TextParser("(")
 val RParenParser = TextParser(")")
 val SpaceParser = TextParser(" ")
 
 object AtomParser : TokenParser {
-    override fun parse(s: String): ParseResult<Token> {
-        val atom = s.takeWhile{
-            LParenParser.value != it.toString() &&
-            RParenParser.value != it.toString() &&
-            SpaceParser.value != it.toString()
-        }
-        return if (atom.isEmpty()) failedParseResult(s) else okParseResult(Atom(atom), s.drop(atom.length))
+    override fun parse(s: String): Token? {
+        val atom = s.takeWhile{ it != '(' && it != ')' && it != ' ' }
+        return if (atom.isEmpty()) null else Atom(atom)
     }
 }
 
 class SequenceParser(val tokenParsers: List<TokenParser>) : TokenParser {
     constructor(vararg tokenParsers: TokenParser) : this(tokenParsers.toList())
 
-    override fun parse(s: String): ParseResult<Token> {
+    override fun parse(s: String): Token? {
         val tokens = ArrayList<Token>()
         var rest = s
         var parsers = tokenParsers
         while (rest.isNotEmpty() && parsers.isNotEmpty()) {
-            val (token, s1) = parsers.first().parse(rest)
-            if (token == null) return failedParseResult(s)
-            if (!token.canDrop()) {
-                tokens.add(token)
-            }
-            rest = s1
+            val token = parsers.first().parse(rest) ?: return null
+            tokens.add(token)
+            rest = rest.drop(token.length())
             parsers = parsers.drop(1)
         }
-        if (tokens.size == 1) return okParseResult(tokens.first(), rest)
-        else return okParseResult(Seq(tokens), rest)
+        if (tokens.size == 1) return tokens.first()
+        else return Seq(tokens)
     }
 }
 
 class RepeatedParser(val tokenParser: TokenParser) : TokenParser {
-    override fun parse(s: String): ParseResult<Seq> {
-        val (token, s1) = tokenParser.parse(s)
-        return when (token) {
-            null -> okParseResult(Seq(), s)
-            else -> {
-                val result = parse(s1)
-                okParseResult(result.token!!.prepend(token), result.s)
-            }
-        }
+    override fun parse(s: String): Seq {
+        val token = tokenParser.parse(s)
+        return if (token == null) Seq()
+        else parse(s.drop(token.length())).prepend(token)
     }
 }
 
 class OrParser(vararg val tokenParsers: TokenParser): TokenParser {
-    override fun parse(s: String): ParseResult<Token> {
-        val result = failedParseResult(s)
+    override fun parse(s: String): Token? {
+        val result = null
         tokenParsers.forEach {
-            val (token, s1) = it.parse(s)
-            if (token != null) return okParseResult(token, s1)
+            val token = it.parse(s)
+            if (token != null) return token
         }
         return result
     }
 }
 
 object SExprParser : TokenParser {
-    override fun parse(s: String): ParseResult<Token> {
+    override fun parse(s: String): Token? {
         val parser = OrParser(
                 SequenceParser(LParenParser, AtomParser, RepeatedParser(SequenceParser(SpaceParser, this)), RParenParser),
                 AtomParser
@@ -136,16 +136,16 @@ class P73Test {
     }
 
     @Test fun `parse s-expression into tokens`() {
-        assertThat(SExprParser.parse("a").token!!, equalTo<Token>(Atom("a")))
-        assertThat(SExprParser.parse("(a)").token!!, equalTo<Token>(Atom("a")))
-        assertThat(SExprParser.parse("(a b c d)").token!!, equalTo<Token>(
+        assertThat(SExprParser.parse("a")!!.trim()!!, equalTo<Token>(Atom("a")))
+        assertThat(SExprParser.parse("(a)")!!.trim()!!, equalTo<Token>(Atom("a")))
+        assertThat(SExprParser.parse("(a b c d)")!!.trim()!!, equalTo<Token>(
             Seq(Atom("a"),
                 Seq(Atom("b"), Atom("c"), Atom("d")))
         ))
-        assertThat(SExprParser.parse("(a b (c d))").token!!, equalTo<Token>(
+        assertThat(SExprParser.parse("(a b (c d))")!!.trim()!!, equalTo<Token>(
             Seq(Atom("a"),
                 Seq(Atom("b"),
-                    Seq(Atom("c"), Seq(Atom("d")))
+                    Seq(Atom("c"), Atom("d"))
                 )
             )
         ))
