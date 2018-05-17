fd9daf0b14a6e311bb22ae0091155792fb5ab281
Uzi Landsmann <uzi.landsmann@gmail.com>
2018-05-17 09:17:22 +0200

    Alternative solution to P16, using filterIndexed

--- src/org/kotlin99/lists/P16.kt
+++ src/org/kotlin99/lists/P16.kt
@@ -4,10 +4,8 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 
-fun <T> drop(n: Int, list: List<T>): List<T> = list
-    .mapIndexed { i: Int, value: T -> Pair(i, value) }
-    .filter { (it.first + 1) % n != 0 }
-    .map { it.second }
+fun <T> drop(n: Int, list: List<T>): List<T> =
+        list.filterIndexed { i, _ -> (i + 1) % n != 0 }
 
 class P16Test {
     @Test fun `drop every Nth element from a list`() {
