ecb9a417c66f044b13089610933fb9e63911b4f3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-08-27 22:56:58 +0100

    kotlin 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4-M1"
+    kotlin("jvm") version "1.4.0"
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
     testImplementation("com.natpryce:hamkrest:1.7.0.2")
     testImplementation("junit:junit:4.13")
 }
@@ -14,6 +12,3 @@ repositories {
     jcenter()
     maven ("https://dl.bintray.com/kotlin/kotlin-eap")
 }
-
-tasks.withType(KotlinCompile::class.java)
-    .forEach { it.kotlinOptions { freeCompilerArgs = listOf("-Xnew-inference") } }
