02d84f5be1e7d2e31f487b35c3e4ee9c75c2d47a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-02-06 15:25:32 +0000

    use new IR and -Xstring-concat=indy-with-constants

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.20"
@@ -11,4 +12,10 @@ dependencies {
 repositories {
     jcenter()
     maven ("https://dl.bintray.com/kotlin/kotlin-eap")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "15"
+    kotlinOptions.useIR = true // see https://kotlinlang.org/docs/reference/whatsnew14.html#new-jvm-ir-backend
+    kotlinOptions.freeCompilerArgs = listOf("-Xstring-concat=indy-with-constants") // see https://kotlinlang.org/docs/reference/whatsnew1420.html
 }
